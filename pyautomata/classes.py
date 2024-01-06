e2f5c5eabca82b7b0f28704d6bfc37965020e32a
Michael Buckley <michaelcbuckley@proton.me>
2024-01-06 18:31:04 -0500

    Initial project commit

---
+++ pyautomata/classes.py
@@ -0,0 +1,65 @@
+# Cellular Automata
+
+from numpy import arange, binary_repr, zeros, ndarray
+
+class Automata:
+    def __init__(self, rule: int) -> None:
+        # Validation
+        init_except_message = 'Rule must be an integer between 1 and 256'
+        if type(rule) != int:
+            raise ValueError(init_except_message)
+        if not 1 <= rule <= 256:
+            raise ValueError(init_except_message)
+        
+        pattern = {}
+        output_pattern = [int(x) for x in binary_repr(rule, width=8)]
+
+        for i in range(8):
+            input_pattern = tuple([int(x) for x in binary_repr(7-i, 3)])
+            pattern[input_pattern] = output_pattern[i]
+
+        self.rule = rule
+        self.pattern = pattern
+
+    def __repr__(self) -> str:
+        return f'Automata: Rule {self.rule}'
+    
+    def get_canvas(self, columns: int = 100) -> 'Canvas':
+        """
+        Method to generate the typical pattern, starting with a single point.
+
+        `columns` is an `int` of the deep that the canvas will be generated to
+        """
+        description = 'Standard center start'
+        return Canvas(self, description, columns)
+
+class Canvas:
+    def __init__(self, automata: Automata, description: str = '', columns: int = 100) -> None:
+        self.columns = columns
+        self.automata = automata
+        self.row_sums = None
+        self.result = None
+        self.description = description
+
+    def __repr__(self) -> str:
+        return f'Canvas: {self.description}'
+        
+    def generate(self) -> ndarray:
+        """
+        
+        """
+        rows = (self.columns//2) + 1
+        canvas = zeros([rows, self.columns+2])
+        canvas[0, int(self.columns/2)+1] = 1
+        self.sums = [1]
+
+        for i in arange(0, rows-1):
+            self.sums.append(0)
+            for j in arange(0, self.columns):
+                local_pattern = tuple(canvas[i, j:j+3])
+                output_pattern = self.automata.pattern.get(local_pattern)
+                canvas[i+1, j+1] = output_pattern
+                self.sums[i+1] += output_pattern
+
+        self.result = canvas
+        return canvas
---
+++ setup.py
@@ -0,0 +1,28 @@
+# Project Python Cellular Automata
+
+# Python Modules
+from setuptools import setup, find_packages
+
+# Local Modules
+from pyautomata.version import VERSION
+
+DESCRIPTION = 'Python Cellular Automata',
+# LONG_DESCRIPTION = DESCRIPTION
+
+# with open('README.md', 'r', encoding='utf-8') as readme:
+#     LONG_DESCRIPTION = readme.read()
+
+setup(
+    name='PyAutomata',
+    author='Michael Buckley',
+    description=DESCRIPTION,
+    # long_description=LONG_DESCRIPTION,
+    # long_description_content_type = 'text/markdown',
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib'
+    ],
+    keywords=['cellular automata']
+)
