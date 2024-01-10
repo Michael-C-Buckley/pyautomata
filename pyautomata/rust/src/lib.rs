a6f32cf9eeda05cb2cb9efd8931ce45296b38b2a
Michael Buckley <michaelcbuckley@proton.me>
2024-01-10 08:49:30 -0500

    Refactor to reduce the scope of the unsafe block

--- pyautomata/rust/src/lib.rs
+++ pyautomata/rust/src/lib.rs
@@ -38,40 +38,40 @@ pub extern "C" fn generate_canvas(
     let mut canvas = vec![0; rows * columns];
     let mut sums = vec![0; rows];
 
-    unsafe {
-        let initial_row_slice = slice::from_raw_parts(initial_row, columns);
-        let rules_slice = slice::from_raw_parts(rules, rules_length);
+    let (initial_row_slice, rules_slice) = unsafe {
+        (
+            slice::from_raw_parts(initial_row, columns),
+            slice::from_raw_parts(rules, rules_length),
+        )
+    };
 
-        let rules_map = process_rules(rules_slice);
+    let rules_map = process_rules(rules_slice);
 
-        let mut first_row_sum: u32 = 0;
-        for (i, &value) in initial_row_slice.iter().enumerate() {
-            assert!(value == 0 || value == 1, "Initial row must contain only 0s and 1s");
-            canvas[i] = value as u8;
-            first_row_sum = first_row_sum + value as u32;
-        }
-        sums[0] = first_row_sum;
+    let mut first_row_sum: u32 = 0;
+    for (i, &value) in initial_row_slice.iter().enumerate() {
+        assert!(value == 0 || value == 1, "Initial row must contain only 0s and 1s");
+        canvas[i] = value as u8;
+        first_row_sum = first_row_sum + value as u32;
+    }
+    sums[0] = first_row_sum;
 
-        for row in 0..rows-1 {
-            let mut row_sum: u32 = 0;
-            for col in 0..columns {
-                let left = if col == 0 { 0 } else { canvas[row * columns + col - 1] };
-                let center = canvas[row * columns + col];
-                let right = if col == columns - 1 { 0 } else { canvas[row * columns + col + 1] };
-        
-                if let Some(&new_value) = rules_map.get(&(left as u8, center as u8, right as u8)) {
-                    canvas[(row + 1) * columns + col] = new_value as u8;
-                    row_sum = row_sum + new_value as u32;
-                }
-            sums[row] = row_sum;
+    for row in 0..rows-1 {
+        let mut row_sum: u32 = 0;
+        for col in 0..columns {
+            let left = if col == 0 { 0 } else { canvas[row * columns + col - 1] };
+            let center = canvas[row * columns + col];
+            let right = if col == columns - 1 { 0 } else { canvas[row * columns + col + 1] };
+    
+            if let Some(&new_value) = rules_map.get(&(left as u8, center as u8, right as u8)) {
+                canvas[(row + 1) * columns + col] = new_value as u8;
+                row_sum = row_sum + new_value as u32;
             }
+        sums[row] = row_sum;
         }
     }
-    
-    // Prevent Rust from freeing the memory when values goes out of scope
+    // Capture the pointers and forget them to prevent automatic memory management
     let canvas_ptr = canvas.as_mut_ptr();
     let sums_ptr = sums.as_mut_ptr();
-
     std::mem::forget(canvas);
     std::mem::forget(sums);
 
@@ -83,9 +83,8 @@ pub extern "C" fn generate_canvas(
 
 #[no_mangle]
 pub extern "C" fn free_memory(ptr: *mut u8, size_in_bytes: usize) {
+    // Function to release the memory after it has been transferred into Python-owned memory
     if !ptr.is_null() {
-        // Reconstruct the Vec from the raw parts
         let _ = unsafe { Vec::from_raw_parts(ptr, size_in_bytes, size_in_bytes) };
-        // Vec gets dropped here, and memory is freed
     }
 }
