b370c9fee6a6eb2070f461d4a262298e9085290d
Michael Buckley <michaelcbuckley@proton.me>
2025-11-08 14:14:09 -0500

    First entry for a cachix for the repository

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   description = "Pyautomata Nix Flake";
 
+  nixConfig = {
+    extra-substituters = [ "https://pyautomata.cachix.org" ];
+    extra-trusted-public-keys = [ "pyautomata.cachix.org-1:VXD1o4jdYUFm5EaWWrO/gm5IqT9w5p6WeTf/GqkVorU=" ];
+  };
+
   outputs = {flake-parts, ...} @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
